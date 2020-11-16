f25dd7cd7a724c9411a39825e240d4ed696971bb
RePloZ <herve.kay@ynov.com>
2020-11-16 11:14:40 +0100

    ➕ Faker

--- src/utils/sample.ts
+++ src/utils/sample.ts
@@ -1,12 +1,13 @@
 import { v4 as uuid } from "uuid";
 import { Article } from "features/news/newsSlice";
+import faker from 'faker';
 
 export const sampleArticle : () => Article = () => ({
     _id: uuid(),
     title: "Titre",
-    image: "Link to image",
-    description: "Description",
-    auteur: "Auteur",
-    publishedAt: Date.now(),
-    content: "Lorem Ipsum ..."
+    image: faker.image.imageUrl(),
+    description: faker.lorem.lines(2),
+    auteur: faker.name.firstName(),
+    publishedAt: new Date(faker.date.recent(1)).getTime(),
+    content: faker.lorem.paragraph(5)
 });
