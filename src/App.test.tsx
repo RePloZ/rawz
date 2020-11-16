0c333cc351a523ca606e062b62da413a1ca1d480
RePloZ <herve.kay@ynov.com>
2020-11-16 01:25:25 +0100

    ✅ new tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,34 +1,64 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
-import { Router } from "react-router-dom";
+import { Router, useHistory } from "react-router-dom";
 import { createMemoryHistory } from 'history'
 import userEvent from '@testing-library/user-event'
 
 import '@testing-library/jest-dom/extend-expect'
+import { selectArticles, setArticles } from 'features/category/categorySlice';
+import { v4 as uuid } from 'uuid';
+import { sampleArticle } from 'utils/sample';
 
-test('Render without any problem', () => {
-  const { getByText } = render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+function wait5second(callback: Function) {
+  setTimeout(() => {
+    callback();
+  }, 5000)
+}
 
-  expect(getByText(/Accueil/i)).toBeInTheDocument();
-});
-
-test('Application rendering/navigating correctly', () => {
-  const history = createMemoryHistory()
-  
-  const { getByText } = render(
-    <Provider store={store}>
-      <Router history={history}>
+describe('Application', () => {
+  it('should render without problem', () => {
+    const { getByText } = render(
+      <Provider store={store}>
         <App />
-      </Router>
-    </Provider>
-  )
+      </Provider>
+    );
+  
+    expect(getByText(/Accueil/i)).toBeInTheDocument();  
+  })
 
-  expect(getByText(/catégorie/i)).toBeInTheDocument()
+  it('should render Application and navigate correctly', () => {
+    const history = createMemoryHistory()
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>
+    )
+  
+    expect(getByText(/catégorie/i)).toBeInTheDocument()
+  })
+
+  xit('should render an article properly', () => {
+    const history = createMemoryHistory()
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <Router history={history}>
+          <App />
+        </Router>
+      </Provider>
+    )
+
+    const articles = new Array(5).fill("").map(() => sampleArticle())
+    setArticles(articles);
+    
+    history.push(`/article/${articles[0]._id}`);
+
+    expect(getByText(/Titre/i)).toBeInTheDocument();
+  })
 })
